52ae9371971edd6f8cd756e5ee0f5de9eb960e49
anompermadi <anom.permadi@outlook.com>
2022-06-12 22:35:51 +0700

    solve using  async await

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  try {
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+    const moodTheaterIXX = theaterIXX.filter((item) => item.hasil === emotion);
+    const moodTheaterVGC = theaterVGC.filter((item) => item.hasil === emotion);
+
+    return moodTheaterIXX.length + moodTheaterVGC.length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
